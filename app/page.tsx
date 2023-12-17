eab26fbe3549f90f5dab52207282eab5a7759f12
jvink10 <jordanvink10@gmail.com>
2023-12-17 20:28:50 +1000

    Add: Clock

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
+import Clock from '../components/Clock';
+
 export default function HomePage() {
-  const [time, setTime] = useState<Array<{continent: string, city: string, year: number, month: number, day: number, hour: number, minute: number, seconds: number, dayOfWeek: string}>>([]);
+  const [time, setTime] = useState<Array<{timeZone: string, time: string, date: string, dayOfWeek: string}>>([]);
 
   useEffect(() => {
     const requestArray = [{ continent: 'Australia', city: 'Brisbane' }, { continent: 'Europe', city: 'Lisbon' }];
@@ -21,7 +23,7 @@ export default function HomePage() {
         const { time } = await response.json();
         setTime(prevTime => {
           const updatedTime = [...prevTime];
-          updatedTime.push({continent: request.continent, city: request.city, year: time.year, month: time.month, day: time.day, hour: time.hour, minute: time.minute, seconds: time.seconds, dayOfWeek: time.dayOfWeek});
+          updatedTime.push(time);
 
           return updatedTime;
         });
@@ -44,9 +46,10 @@ export default function HomePage() {
         </div>
       </header>
       <section id="linkSection">
-        <div>
-          <p>The time in Brisbane is {time.find((data) => data.continent === "Australia" && data.city === "Brisbane")?.dayOfWeek}</p>
-          <p>The time in Lisbon is {time.find((data) => data.continent === "Europe" && data.city === "Lisbon")?.dayOfWeek}</p>
+        <div className="flex flex-row mx-auto my-16 rounded-2xl w-fit bg-zinc-300">
+          {time.map((timeData, index) => (
+            <Clock key={index} timeData={timeData} />
+          ))}
         </div>
       </section>
     </main>
