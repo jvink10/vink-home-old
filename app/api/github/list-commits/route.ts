d0dfc525d798877a55969c513d34c84e6a76acdd
jvink10 <jordanvink10@gmail.com>
2023-12-18 02:23:32 +1000

    Update: Token security

--- app/api/github/list-commits/route.ts
+++ app/api/github/list-commits/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
 	const repo = searchParams.get('repo');
 
 	const octokit = new Octokit({
-		auth: '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'
+		auth: process.env.REPO_READ_TOKEN
 	});
 	const response = await octokit.request(`GET /repos/${owner}/${repo}/commits`, {
 		owner: owner,
